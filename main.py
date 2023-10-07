6007ed7080a5f3ded6eea14fea38ad10ca5d0fa9
LIN <fredericlin2001@gmail.com>
2023-10-07 02:19:16 +0200

    baseline with image

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import torch
 from dataset import EpisodicSampler, FolderExplorer
 from model import get_model
 from config import cfg  # cfg.paths is a list of paths to the datasets
+from ncm import NCM
 from PIL import Image
 from torchvision import transforms as T
 from tqdm import tqdm
+
 
 def main(cfg):
     folder_explorer = FolderExplorer(cfg.paths)
@@ -15,82 +17,95 @@ def main(cfg):
                               n_query= cfg.sampler.n_queries,
                               n_ways = cfg.sampler.n_ways,
                               n_shot = cfg.sampler.n_shots,)
+    acc = 0
+    for i in tqdm(range(10)):
     
-    episode = sampler() #episode is (dataset, classe, support/query, image_path)
-    
-    imagenet_sample = episode["imagenet"]
-    
+        episode = sampler() #episode is (dataset, classe, support/query, image_path)
 
-    
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = get_model(size="s",use_v2=False)
-    model.to(device)
-    model.eval()
-    
-    transforms = T.Compose([
-        T.ToTensor(),
-        T.Resize(224, antialias=True), # generates warning otherwise
-        T.CenterCrop(224),
-        T.Normalize(mean=[0.485,0.456,0.406],
-                    std=[0.229,0.224,0.225]) # imagenet mean and std
-    ])
-    
-    with torch.inference_mode():
-        # compute features for support set
-        support_features = {}
-        support_images = [image_path for classe in imagenet_sample for image_path in imagenet_sample[classe]["support"]]
-        support_classes = [classe for classe in imagenet_sample for image_path in imagenet_sample[classe]["support"]]
+        imagenet_sample = episode["imagenet"]
 
-        # batch 
-        support_batch = [support_images[i:i+cfg.batch_size] for i in range(0,len(support_images),cfg.batch_size)]
-        classes_batch = [support_classes[i:i+cfg.batch_size] for i in range(0,len(support_classes),cfg.batch_size)]
-        
-        for batch, class_batch in tqdm(zip(support_batch,classes_batch), total=len(support_batch)):
+
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = get_model(size="s",use_v2=False)
+        model.to(device)
+        model.eval()
+
+        transforms = T.Compose([
+            T.ToTensor(),
+            T.Resize(224, antialias=True), # generates warning otherwise
+            T.CenterCrop(224),
+            T.Normalize(mean=[0.485,0.456,0.406],
+                        std=[0.229,0.224,0.225]) # imagenet mean and std
+        ])
+
+        with torch.inference_mode():
+            # compute features for support set
+            support_features = {}
+            support_images = [image_path for classe in imagenet_sample for image_path in imagenet_sample[classe]["support"]]
+            support_classes = [classe for classe in imagenet_sample for image_path in imagenet_sample[classe]["support"]]
+            # batch 
+            support_batch = [support_images[i:i+cfg.batch_size] for i in range(0,len(support_images),cfg.batch_size)]
+            classes_batch = [support_classes[i:i+cfg.batch_size] for i in range(0,len(support_classes),cfg.batch_size)]
             
-            images = [Image.open(image_path).convert("RGB") for image_path in batch] # png have 4 channels
-            batch_images = torch.stack([transforms(image) for image in images]).to(device) # bs,3,224,224
-            
-            outputs = model(batch_images)
-            
-            # unpack outputs
-            for i,classe in enumerate(class_batch):
-                if classe not in support_features:
-                    support_features[classe] = outputs[i].unsqueeze(0)
-                else:
-                    support_features[classe] = torch.cat([support_features[classe],outputs[i].unsqueeze(0)],dim=0)
-        # compute features for query set
-        query_features = {}
-        query_images = [image_path for classe in imagenet_sample for image_path in imagenet_sample[classe]["query"]]
-        query_classes = [classe for classe in imagenet_sample for image_path in imagenet_sample[classe]["query"]]
-        
-        # batch
-        query_batch = [query_images[i:i+cfg.batch_size] for i in range(0,len(query_images),cfg.batch_size)]
-        classes_batch = [query_classes[i:i+cfg.batch_size] for i in range(0,len(query_classes),cfg.batch_size)]
-        
-        for batch, class_batch in tqdm(zip(query_batch,classes_batch), total=len(query_batch)):
-            
-            images = [Image.open(image_path).convert("RGB") for image_path in batch]
-            batch_images = torch.stack([transforms(image) for image in images]).to(device)
-            
-            outputs = model(batch_images)
-            
-            # unpack outputs
-            
-            for i,classe in enumerate(class_batch):
-                if classe not in query_features:
-                    query_features[classe] = outputs[i].unsqueeze(0)
-                else:
-                    query_features[classe] = torch.cat([query_features[classe],outputs[i].unsqueeze(0)],dim=0)
+            for batch, class_batch in zip(support_batch,classes_batch):
+                
+                images = [Image.open(image_path).convert("RGB") for image_path in batch] # png have 4 channels
+                batch_images = torch.stack([transforms(image) for image in images]).to(device) # bs,3,224,224
+                
+                outputs = model(batch_images)
+                
+                # unpack outputs
+                for i,classe in enumerate(class_batch):
+                    if classe not in support_features:
+                        support_features[classe] = {"features": torch.empty(0,dtype=torch.float32,device=device),
+                                                    "indices": torch.empty(0,dtype=torch.int64,device=device)}
+                    support_features[classe]["features"] = torch.cat((support_features[classe]["features"],outputs[i].unsqueeze(0)),dim=0)
+                    index = len(support_features[classe]["features"])-1
+                    support_features[classe]["indices"] = torch.cat((support_features[classe]["indices"],
+                                                                    torch.tensor([index],dtype=torch.int64,device=device)),dim=0)
                     
-        print(support_features.keys())
-        print(query_features.keys())
-        
-        print("J'ai fini ma partie à vous de jouer")
+                    
+            # compute features for query set
+            query_features = {}
+            query_images = [image_path for classe in imagenet_sample for image_path in imagenet_sample[classe]["query"]]
+            query_classes = [classe for classe in imagenet_sample for image_path in imagenet_sample[classe]["query"]]
+            
+            # batch
+            query_batch = [query_images[i:i+cfg.batch_size] for i in range(0,len(query_images),cfg.batch_size)]
+            classes_batch = [query_classes[i:i+cfg.batch_size] for i in range(0,len(query_classes),cfg.batch_size)]
+            
+            for batch, class_batch in zip(query_batch,classes_batch):
+                
+                images = [Image.open(image_path).convert("RGB") for image_path in batch]
+                batch_images = torch.stack([transforms(image) for image in images]).to(device)
+                
+                outputs = model(batch_images)
+                
+                # unpack outputs
+                
+                for i,classe in enumerate(class_batch):
+                    if classe not in query_features:
+                        query_features[classe] = {"features": torch.empty(0,dtype=torch.float32,device=device),
+                                                    "indices": torch.empty(0,dtype=torch.int64,device=device)}
+                    query_features[classe]["features"] = torch.cat((query_features[classe]["features"],outputs[i].unsqueeze(0)),dim=0)
+                    index = len(query_features[classe]["features"])-1
+                    query_features[classe]["indices"] = torch.cat((query_features[classe]["indices"],
+                                                                    torch.tensor([index],dtype=torch.int64,device=device)),dim=0)
+
+            
+            ncm = NCM()
+            run_acc = ncm(support_features, query_features)
+            print("Accuracy: ", run_acc)
+            acc += run_acc
+    print("Average accuracy: ", acc/10)
         
         ## TO DO: pass the features to the NCM model
         
         # structure of support_features and query_features:
-        # {classe : tensor of features (n_shot or n_query, d)} (tensors on gpu)
+        # {classe : {"features": tensor([n_shot, d]), "indices": tensor([n_shot])}, ...
+        #  classe : {"features": tensor([n_query, d]), "indices": tensor([n_query])}, ...}
+        # put indices because we need to know which feature corresponds to which image when including the masks
         
                 
 if __name__ == "__main__":
--- ncm.py
+++ ncm.py
@@ -5,66 +5,65 @@ class preprocess_NCM(torch.nn.Module):
     # preprocess_NCM: preprocess the features for NCM
     # including: 1. subtract the mean feature vector
     #            2. normalize the feature vector to make it into hypersphere
-    # input: support_features: [n_way, n_shot, d]
-    #        query_features: [n_query, d]
-    # output: sphered_support_features: [n_way, n_shot, d]
-    #         sphered_query_features: [n_query, d]
+    # input: support_features: {class1: {features: [n_shot, d], indices: [n_shot]}, ...}
+    #        query_features: {class1: {features: [n_query, d], indices: [n_query]}, ...}
+    # output: support_output: [(class,index (shot),feature), ...]
+    #         sphered_query_features: [(class,index (query),feature), ...]
     def __init__(self):
         super(preprocess_NCM, self).__init__()
     def forward(self, support_features, query_features):
-        mean_features = torch.mean(support_features, dim=[0,1],keepdim=True) # [1, 1, d]
-        support_features = support_features - mean_features # [n_way, n_shot, d]
-        query_features = query_features - mean_features.squeeze(0) # [n_query, d]
-        sphered_support_features = support_features/torch.norm(support_features, dim=2, keepdim=True)
-        sphered_query_features = query_features/torch.norm(query_features, dim=1, keepdim=True)
-        return sphered_support_features, sphered_query_features
-    
-
+        all_support_features = []
+        for class_id in support_features:
+            all_support_features.extend(support_features[class_id]["features"]) # [n_way * n_shot, d], n_shot may be different for each class
+        mean_feature = torch.mean(torch.stack(all_support_features), dim=0) # [d]
+        sphered_support_features = {} # {class: [n_shot, d], ...}
+        sphered_query_features = {} # {class: [n_query, d], ...}
+        for class_label, class_features in support_features.items():
+            sphered_support_features[class_label] = class_features["features"] - mean_feature
+            sphered_support_features[class_label] = F.normalize(sphered_support_features[class_label], p=2, dim=1) # [n_shot, d]
+            sphered_support_features[class_label] = {"features": sphered_support_features[class_label], "indices": class_features["indices"]}
+        for class_label, class_features in query_features.items():
+            sphered_query_features[class_label] = class_features["features"] - mean_feature
+            sphered_query_features[class_label] = F.normalize(sphered_query_features[class_label], p=2, dim=1) # [n_query, d]
+            sphered_query_features[class_label] = {"features": sphered_query_features[class_label], "indices": class_features["indices"]}
+        support_output = [] # [(class,index,feature), ...]
+        query_output = [] # [(class,index,feature), ...]
+        for class_label, class_features in sphered_support_features.items():
+            for index in class_features["indices"]:
+                support_output.append((class_label, index, class_features["features"][index]))
+        for class_label, class_features in sphered_query_features.items():
+            for index in class_features["indices"]:
+                query_output.append((class_label, index, class_features["features"][index]))
+        return support_output, query_output
 class NCM(torch.nn.Module):
     def __init__(self):
         super(NCM, self).__init__()
 
     def forward(self, support_features, query_features, use_cosine=True):
-        # support_features: [n_way, n_shot, d]
-        # query_features: [n_query, d]
-        # output: [n_way] each element is the predicted class for each query
+        # support_features: [(class,shot,feature), ...)]
+        # query_features: [(class,query,feature), ...)]
+        # output: [n_query,class] each element is the predicted class for each query
 
         # Preprocess the features
         preprocess_NCM_layer = preprocess_NCM()
         support_features, query_features = preprocess_NCM_layer(support_features, query_features)
-
-        if use_cosine: 
-            # Reshape query_features for broadcasting
-            query_features = query_features.unsqueeze(1)  # [n_query, 1, d] 
-            query_features = query_features.unsqueeze(0)        # [1, n_query, 1, d]
-            support_features = support_features.unsqueeze(1)    # [n_way, 1, n_shot, d]
-
-            # torch.sum works only if the dimensions are the same or one of them is 1 (broadcasting)
-    
-            # Calculate cosine similarities
-            cosine_similarities = torch.sum(support_features * query_features, dim=3)  # [n_way, n_query, n_shot]
-
-            # Calculate the best similarity for each class
-            best_similarities = torch.max(cosine_similarities, dim=2)[0] # [n_way, n_query]
-
-            # Calculate the best class for each query
-            output = torch.argmax(best_similarities, dim=0) # [n_way]
-
-            return output
-    
-        else:
-            # use torch.cdist to calculate the euclidean distance
-
-            # calculate the euclidean distance
-            euclidean_distance = torch.cdist(query_features, support_features, p=2.0)
-            
-            # calculate the best distance for each class
-            best_distance = torch.min(euclidean_distance, dim=2)[0]
-
-            # calculate the best class for each query
-            output = torch.argmin(best_distance, dim=0)
-
-            return output
+        acc = 0
+        for query_class, query_index, query_feature in query_features:
+            max_sim = float("-inf")
+            max_class = None
+            for support_class, support_index, support_feature in support_features:
+                if use_cosine:
+                    sim = torch.dot(query_feature, support_feature)
+                else:
+                    sim = -torch.norm(query_feature - support_feature, p=2)
+                if sim > max_sim:
+                    max_sim = sim
+                    max_class = support_class
+            if max_class == query_class:
+                acc += 1
+        acc = acc / len(query_features)
+        return acc
+        
 
 
 
