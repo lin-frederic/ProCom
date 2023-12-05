7283d4f10a00515aac4ee0816ec28641d16710b6
Jules <j21decae@sl-tp-br-510.imta.fr>
2023-12-05 20:55:22 +0100

    ♻️ refactor preprocess_features for ncm and transductive

--- classif/ncm.py
+++ classif/ncm.py
@@ -1,28 +1,11 @@
 import torch
 import torch.nn.functional as F
+from tools import preprocess_Features
 
-class preprocess_NCM(torch.nn.Module):
-    # preprocess_NCM: preprocess the features for NCM
-    # including: 1. subtract the mean feature vector
-    #            2. normalize the feature vector to make it into hypersphere
-    # input: support_features: list of features, as a tensor
-    #        query_features: list of features, as a tensor
-    # output: support_output: list of features, as a tensor
-    #         sphered_query_features: list of features, as a tensor
-    def __init__(self):
-        super(preprocess_NCM, self).__init__()
-    def forward(self, support_features, query_features):
-        mean_feature = torch.mean(support_features, dim=0) # [d]
-        sphered_support_features = support_features - mean_feature
-        sphered_support_features = F.normalize(sphered_support_features, p=2, dim=1) # [n_shot, d]
-        sphered_query_features = query_features - mean_feature
-        sphered_query_features = F.normalize(sphered_query_features, p=2, dim=1) # [n_query, d]
-        
-        return sphered_support_features, sphered_query_features
 class NCM(torch.nn.Module):
     def __init__(self):
         super(NCM, self).__init__()
-        self.preprocess_NCM_layer = preprocess_NCM()
+        self.preprocess_NCM_layer = preprocess_Features()
 
     def forward(self, support_features, query_features, support_labels, query_labels, use_cosine=False):
         # support_features: list of features, as a tensor of shape [n_shot, d]
