0e6e002da59b98b5b0603bbdcdaaa8b3decf04d2
LIN <fredericlin2001@gmail.com>
2023-12-05 20:37:13 +0100

    ncm changing architecture

--- ncm.py
+++ ncm.py
@@ -5,62 +5,58 @@ class preprocess_NCM(torch.nn.Module):
     # preprocess_NCM: preprocess the features for NCM
     # including: 1. subtract the mean feature vector
     #            2. normalize the feature vector to make it into hypersphere
-    # input: support_features: {class1: {features: [n_shot, d], indices: [n_shot]}, ...}
-    #        query_features: {class1: {features: [n_query, d], indices: [n_query]}, ...}
-    # output: support_output: [(class,index (shot),feature), ...]
-    #         sphered_query_features: [(class,index (query),feature), ...]
+    # input: support_features: list of features, as a tensor
+    #        query_features: list of features, as a tensor
+    # output: support_output: list of features, as a tensor
+    #         sphered_query_features: list of features, as a tensor
     def __init__(self):
         super(preprocess_NCM, self).__init__()
     def forward(self, support_features, query_features):
-        all_support_features = []
-        for class_id in support_features:
-            all_support_features.extend(support_features[class_id]["features"]) # [n_way * n_shot, d], n_shot may be different for each class
-        mean_feature = torch.mean(torch.stack(all_support_features), dim=0) # [d]
-        sphered_support_features = {} # {class: [n_shot, d], ...}
-        sphered_query_features = {} # {class: [n_query, d], ...}
-        for class_label, class_features in support_features.items():
-            sphered_support_features[class_label] = class_features["features"] - mean_feature
-            sphered_support_features[class_label] = F.normalize(sphered_support_features[class_label], p=2, dim=1) # [n_shot, d]
-            sphered_support_features[class_label] = {"features": sphered_support_features[class_label], "indices": class_features["indices"]}
-        for class_label, class_features in query_features.items():
-            sphered_query_features[class_label] = class_features["features"] - mean_feature
-            sphered_query_features[class_label] = F.normalize(sphered_query_features[class_label], p=2, dim=1) # [n_query, d]
-            sphered_query_features[class_label] = {"features": sphered_query_features[class_label], "indices": class_features["indices"]}
-        support_output = [] # [(class,index,feature), ...]
-        query_output = [] # [(class,index,feature), ...]
-        for class_label, class_features in sphered_support_features.items():
-            for index in class_features["indices"]:
-                support_output.append((class_label, index, class_features["features"][index]))
-        for class_label, class_features in sphered_query_features.items():
-            for index in class_features["indices"]:
-                query_output.append((class_label, index, class_features["features"][index]))
-        return support_output, query_output
+        mean_feature = torch.mean(support_features, dim=0) # [d]
+        sphered_support_features = support_features - mean_feature
+        sphered_support_features = F.normalize(sphered_support_features, p=2, dim=1) # [n_shot, d]
+        sphered_query_features = query_features - mean_feature
+        sphered_query_features = F.normalize(sphered_query_features, p=2, dim=1) # [n_query, d]
+        
+        return sphered_support_features, sphered_query_features
 class NCM(torch.nn.Module):
     def __init__(self):
         super(NCM, self).__init__()
+        self.preprocess_NCM_layer = preprocess_NCM()
 
-    def forward(self, support_features, query_features, use_cosine=True):
-        # support_features: [(class,shot,feature), ...)]
-        # query_features: [(class,query,feature), ...)]
+    def forward(self, support_features, query_features, support_labels, query_labels, use_cosine=False):
+        # support_features: list of features, as a tensor of shape [n_shot, d]
+        # support_labels: list of (class,image_index) tuples
+        # query_features: list of features, as a tensor of shape [n_query, d]
         # output: [n_query,class] each element is the predicted class for each query
-
         # Preprocess the features
-        preprocess_NCM_layer = preprocess_NCM()
-        support_features, query_features = preprocess_NCM_layer(support_features, query_features)
+        
+        support_features, query_features = self.preprocess_NCM_layer(support_features, query_features)
         acc = 0
-        for query_class, query_index, query_feature in query_features:
-            max_sim = float("-inf")
-            max_class = None
-            for support_class, support_index, support_feature in support_features:
-                if use_cosine:
-                    sim = torch.dot(query_feature, support_feature)
-                else:
-                    sim = -torch.norm(query_feature - support_feature, p=2)
-                if sim > max_sim:
-                    max_sim = sim
-                    max_class = support_class
-            if max_class == query_class:
-                acc += 1
+        # calculate similarity between each query feature and each support feature
+        if use_cosine:
+            similarity = F.cosine_similarity(query_features.unsqueeze(1), support_features.unsqueeze(0), dim=2) # [n_query, n_shot]
+        else:
+            similarity = -torch.cdist(query_features.unsqueeze(1), support_features.unsqueeze(0), p=2)
+        #similarity is (n_query, n_shot)
+        # problem is we can have multiple features per image (as we have multiple masks)
+        # for each query, slice the similarity tensor to get the similarity with the support features of the same class
+        # then take the max similarity
+        similarity = similarity.squeeze(1)
+        visited = set()
+        for i, (query_class, query_index) in enumerate(query_labels):
+            #find where the other masks of the same image are
+            if query_index not in visited:
+                visited.add(query_index)
+                indices = [j for j, (class_, index_) in enumerate(query_labels) if index_ == query_index]
+                query_similarity = similarity[indices, :] # [n_masks, n_shot]
+                support_similarity, support_indices = torch.max(query_similarity, dim=1) # [n_masks]
+                # this would be the predicted class for each mask
+                # now we take the highest similarity
+                mask_sim, mask_index = torch.max(support_similarity, dim=0)
+                pred_idx = support_indices[mask_index]
+                pred_class = support_labels[pred_idx][0]
+                acc += int(pred_class == query_class)
         acc = acc / len(query_features)
         return acc
         
@@ -70,13 +66,12 @@ class NCM(torch.nn.Module):
 def test():
     # Test NCM
     ncm = NCM()
-    support_features = torch.randn(4, 6, 10)
-    query_features = torch.randn(15, 10)
-    output = ncm(support_features, query_features, False)
-    print("Euclidean distance: ", output)
-    output = ncm(support_features, query_features)
-    print("Cosine similarity: ", output)
-
+    support_features = torch.randn(5*2*3, 10)
+    query_features = torch.randn(15*5*3, 10)
+    support_labels = [(i//(2*3), i//3) for i in range(5*2*3)]
+    query_labels = [(i//(15*3), i//3) for i in range(15*5*3)]
+    acc = ncm(support_features, query_features, support_labels, query_labels)
+    print(acc)
 
 if __name__ == '__main__':
     test()
